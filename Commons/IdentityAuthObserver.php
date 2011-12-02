dd9d4ff23047038b0104c7bcf588498555c92c3e
Andreína Romero <andreina.romero@solucioneshospedadas.com>
2011-12-01 19:58:46 -0430

    Detectado el evento a programar en el observador

--- Commons/IdentityAuthObserver.php
+++ Commons/IdentityAuthObserver.php
@@ -1,6 +1,7 @@
 <?php
 namespace Guzzle\Openstack\Commons;
-
+use Guzzle\Common\Event\Observer;
+use Guzzle\Common\Event\Subject;
 /**
  * Observador que está escuchando el evento before_send para determinar si el token está vencido
  *
@@ -9,8 +10,11 @@ namespace Guzzle\Openstack\Commons;
 class IdentityAuthObserver implements Observer{
     public function update(Subject $subject, $event, $context = null)
     {
-        if ($event == 'request.before_send') {
-            
+        if ($event == 'request.bad_response') {
+            //Se debe verificar si el bad_response es el 401
+            echo "Holaaaaaa";
+            echo $subject->getResponse();
+           
             
         }
     }
