f8fbb58e03ce1634e309c98e66c9def12fdb36e1
Adrian Moya <adrianmoya@gmail.com>
2012-01-12 21:33:16 -0430

    List Users command

---
+++ Identity/Command/ListUsers.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @license See the LICENSE file that was distributed with this source code.
+ */
+
+namespace Guzzle\Openstack\Identity\Command;
+
+use Guzzle\Openstack\Common\AbstractJsonCommand;
+
+/**
+ * List users
+ *
+ * @guzzle marker doc="Marker for pagination"
+ * @guzzle limit doc="Limit for pagination"
+ */
+class ListUsers extends AbstractJsonCommand
+{
+    /**
+     * Set the marker for pagination
+     *
+     * @param string $marker
+     *
+     * @return ListUsers
+     */
+    public function setMarker($marker)
+    {
+        return $this->set('marker', $marker);
+    }
+
+    /**
+     * Set the limit for pagination
+     *
+     * @param integer $limit
+     *
+     * @return ListUsers
+     */
+    public function setLimit($limit)
+    {
+        return $this->set('limit', $limit);
+    }
+
+    protected function build()
+    {
+        if($this->hasKey('marker')){
+            $this->request->getQuery()->set('marker', $this->get('marker'));
+        }
+
+        if($this->hasKey('limit')){
+            $this->request->getQuery()->set('limit', $this->get('limit'));
+        }
+        
+        $this->request = $this->client->get('users', array("X-Auth-Token" => $this->client->getIdentity()->getToken($this->client->getUsername(), $this->client->getPassword())));
+    }
+}
--- Tests/Identity/Command/ListUsersTest.php
+++ Tests/Identity/Command/ListUsersTest.php
@@ -5,20 +5,20 @@ namespace Guzzle\Openstack\Tests\Identity\Command;
 /**
  * List Tenants command unit test
  */
-class ListTenantsTest extends \Guzzle\Tests\GuzzleTestCase
+class ListUsersTest extends \Guzzle\Tests\GuzzleTestCase
 {
 
-    public function testListTenants()
+    public function testListUsers()
     {
         $authclient = \Guzzle\Openstack\IdentityAuth\IdentityAuthClient::factory(array('username' => 'username', 'password' => 'password', 'ip' => '192.168.4.100'));
         $client = \Guzzle\Openstack\Identity\IdentityClient::factory(array('identity' => $authclient, 'username'=>'username', 'password'=>'password'));
         $this->setMockResponse($client->getIdentity(), 'identity_auth/AuthenticateAuthorized');        
-        $this->setMockResponse($client, 'identity/ListTenants');        
-        $command = $client->getCommand('ListTenants');
+        $this->setMockResponse($client, 'identity/ListUsers');        
+        $command = $client->getCommand('ListUsers');
         $command->prepare();
       
         //Check method and resource
-        $this->assertEquals('http://192.168.4.100:5000/v2.0/tenants', $command->getRequest()->getUrl());
+        $this->assertEquals('http://192.168.4.100:5000/v2.0/users', $command->getRequest()->getUrl());
         $this->assertEquals('GET', $command->getRequest()->getMethod());
                 
         //Check for authentication header
@@ -29,7 +29,7 @@ class ListTenantsTest extends \Guzzle\Tests\GuzzleTestCase
         $result = $command->getResult();
         $this->assertTrue(is_array($result));
         
-        $this->assertTrue(array_key_exists('tenants', $result));
+        $this->assertTrue(array_key_exists('users', $result));
         
     }
 }
