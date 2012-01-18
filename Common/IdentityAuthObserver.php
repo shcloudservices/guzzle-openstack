d78da4a2082f40c41083e533cfe73dc6d69eec7c
Michael Dowling <mtdowling@gmail.com>
2012-01-18 00:09:06 -0600

    Updating the Identity observer.  Changing the implementation to use the new onComplete callback feature of overriding a response

--- Common/IdentityAuthObserver.php
+++ Common/IdentityAuthObserver.php
@@ -5,28 +5,50 @@
 
 namespace Guzzle\Openstack\Common;
 
+use Guzzle\Common\Event;
+
 /**
  * Observer to manage authentication for Openstack Clients
  */
 class IdentityAuthObserver implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
 {
-
     public static function getSubscribedEvents()
     {
         return array(
-            'client.create_request' => 'onRequestCreate'
+            'client.create_request' => 'onRequestCreate',
+            'command.before_send' => 'onCommandBeforeSend',
         );
-    }    
-    
-    public function onRequestCreate(\Guzzle\Common\Event $event)
-    {               
-        $subject = $event['client'];
-        $context = $event['request'];
-        $username = $subject->getUsername();
-        $password = $subject->getPassword();
-        $token = $subject->getIdentity()->getToken($username, $password);
-        $context->setHeader('X-Auth-Token', $token);
+    }
+
+    public function onRequestCreate(Event $event)
+    {
+        $token = $event['client']->getIdentity()->getToken($client->getUsername(), $client->getPassword());
+        $event['request']->setHeader('X-Auth-Token', $token)
+
+        $event['request']->setOnComplete(function($request, $response, $default) {
+
+            // Automatically retry failed once
+            if ($response->getStatusCode() == 401 && !$request->getParams()->get('auth_retry')) {
+                $client = $request->getParams()->get('command')->getClient();
+                $token = $client->getIdentity()->getToken($client->getUsername(), $client->getPassword());
+                $cloned = clone $request;
+                $cloned->getParams()->set('auth_retry', 1);
+                $cloned->setHeader('X-Auth-Token', $token);
+                return $cloned->send();
+            }
+
+            call_user_func($default, $request, $response);
+        });
+    }
+
+    /**
+     * Associate the command with the request when it is created
+     *
+     * @param Event $event
+     */
+    public function onCommandBeforeSend(Event $event)
+    {
+        $command = $event['command'];
+        $command->getRequest()->getParams()->set('command', $command);
     }
 }
-
-?>
