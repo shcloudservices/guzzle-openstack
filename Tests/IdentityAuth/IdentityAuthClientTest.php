91f86ffcb6d4ed3652338cc7fd0c30e082ca2048
Adrian Moya <adrianmoya@gmail.com>
2012-01-18 21:13:42 -0430

    Added unit test of autorefresh token feature

--- Tests/IdentityAuth/IdentityAuthClientTest.php
+++ Tests/IdentityAuth/IdentityAuthClientTest.php
@@ -13,6 +13,7 @@ class IdentityAuthClientTest extends \Guzzle\Tests\GuzzleTestCase
         $client = $this->getServiceBuilder()->get('test.identityauth');
         $this->setMockResponse($client, 'identity_auth/AuthenticateUnauthorized'); 
         
+        //Check exception when unauthorized
         $this->setExpectedException('Guzzle\Openstack\Common\OpenstackException');
         $client->getToken('username','password');
         
@@ -25,7 +26,11 @@ class IdentityAuthClientTest extends \Guzzle\Tests\GuzzleTestCase
         $this->setMockResponse($client, array('identity_auth/AuthenticateUnauthorized','identity_auth/AuthenticateAuthorized')); 
         $token = $client->getToken('username','password');
         $this->assertEquals('admintoken', $token);
-        
+
+        //Check exception if double unauthorized
+        $this->setMockResponse($client, array('identity_auth/AuthenticateUnauthorized','identity_auth/AuthenticateUnauthorized')); 
+        $this->setExpectedException('Guzzle\Openstack\Common\OpenstackException');
+        $client->getToken('username','password');        
     }
 }
 ?>
