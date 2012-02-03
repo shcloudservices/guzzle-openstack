3bbd4a5716017af4f7d4092f2b3c865768c061a1
Andreína Romero <andreinar@gmail.com>
2012-02-02 21:35:01 -0430

    Herencia en el comando ListUser para que herede los parámetros de paginación, al parecer esto falla

---
+++ Common/Command/Paginator.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @license See the LICENSE file that was distributed with this source code.
+ */
+namespace Guzzle\Openstack\Common\Command;
+
+use Guzzle\Openstack\Common\AbstractJsonCommand;
+/**
+ * Commons functions of pagination in the commands
+ *
+ * @guzzle marker doc="Marker for pagination"
+ * @guzzle limit doc="Limit for pagination"
+ */
+class Paginator extends AbstractJsonCommand{
+    /**
+     * Set the marker for pagination
+     *
+     * @param string $marker
+     *
+     * @return ListUsers
+     */
+    public function setMarker($marker)
+    {
+        return $this->set('marker', $marker);
+    }
+
+    /**
+     * Set the limit for pagination
+     *
+     * @param integer $limit
+     *
+     * @return ListUsers
+     */
+    public function setLimit($limit)
+    {
+        return $this->set('limit', $limit);
+    }
+    
+    protected function build()
+    {
+        if($this->hasKey('marker')){
+            echo "MARKER" . $this->get('marker');
+            $this->request->getQuery()->set('marker', $this->get('marker'));
+        }
+
+        if($this->hasKey('limit')){
+            $this->request->getQuery()->set('limit', $this->get('limit'));
+        }
+    }
+}
+
+?>
--- Tests/Identity/Command/ListUsersTest.php
+++ Tests/Identity/Command/ListUsersTest.php
@@ -8,13 +8,20 @@ namespace Guzzle\Openstack\Tests\Identity\Command;
 class ListUsersTest extends \Guzzle\Tests\GuzzleTestCase
 {
 
+    public function setUp()
+    {
+        $authclient = \Guzzle\Openstack\Authentication\AuthenticationClient::factory(array('username' => 'username', 'password' => 'password', 'ip' => '192.168.4.100', 'port'=>'35357'));
+        $this->client = \Guzzle\Openstack\Identity\IdentityClient::factory(array('identity' => $authclient, 'username'=>'username', 'password'=>'password'));        
+        $this->setMockResponse($this->client->getIdentity(), 'authentication/AuthenticateAuthorized');                
+    }
+    
     public function testListUsers()
     {
         $authclient = \Guzzle\Openstack\Authentication\AuthenticationClient::factory(array('username' => 'username', 'password' => 'password', 'ip' => '192.168.4.100'));
-        $client = \Guzzle\Openstack\Identity\IdentityClient::factory(array('identity' => $authclient, 'username'=>'username', 'password'=>'password'));
-        $this->setMockResponse($client->getIdentity(), array('authentication/AuthenticateAuthorized'));  
-        $this->setMockResponse($client, 'identity/ListUsers');        
-        $command = $client->getCommand('ListUsers');
+        $this->client = \Guzzle\Openstack\Identity\IdentityClient::factory(array('identity' => $authclient, 'username'=>'username', 'password'=>'password'));
+        $this->setMockResponse($this->client->getIdentity(), array('authentication/AuthenticateAuthorized'));  
+        $this->setMockResponse($this->client, 'identity/ListUsers');        
+        $command = $this->client->getCommand('ListUsers');
         $command->prepare();
       
         //Check method and resource
@@ -24,7 +31,7 @@ class ListUsersTest extends \Guzzle\Tests\GuzzleTestCase
         //Check for authentication header
         $this->assertTrue($command->getRequest()->hasHeader('X-Auth-Token'));
                         
-        $client->execute($command);
+        $this->client->execute($command);
       
         $result = $command->getResult();
         $this->assertTrue(is_array($result));
@@ -32,4 +39,14 @@ class ListUsersTest extends \Guzzle\Tests\GuzzleTestCase
         $this->assertTrue(array_key_exists('users', $result));
         
     }
+    
+    public function testMarkerParameter() {
+        $command = $this->client->getCommand('ListUsers', array("marker" => "1979"));
+        $this->setMockResponse($this->client->getIdentity(), array('authentication/AuthenticateAuthorized'));  
+        $this->setMockResponse($this->client, 'identity/ListUsers'); 
+        $command->prepare();
+        $this->assertEquals('http://192.168.4.100:5000/v2.0/users/marker=1979', $command->getRequest()->getUrl());
+    }
+    
+    
 }
