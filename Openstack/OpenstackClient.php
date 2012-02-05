da88f2c029fedc5cc592783371d50b67daf76b28
Adrian Moya <adrianmoya@gmail.com>
2012-02-04 23:53:20 -0430

    Working on OS Client

--- Openstack/OpenstackClient.php
+++ Openstack/OpenstackClient.php
@@ -3,6 +3,8 @@
 namespace Guzzle\Openstack\Openstack;
 
 use \Guzzle\Service\Inspector;
+use Guzzle\Openstack\Identity\IdentityClient;
+use Guzzle\Openstack\Compute\ComputeClient;
 
 /**
  * @license See the LICENSE file that was distributed with this source code.
@@ -11,11 +13,11 @@ use \Guzzle\Service\Inspector;
 /**
  * Openstack Client
  */
-class OpenstackClient extends \Guzzle\Service\Client{
-    
-     protected $username, $password, $tenantName, $token;
-     protected $computeClient, $identityClient, $serviceCatalog;
-     
+class OpenstackClient extends \Guzzle\Service\Client {
+
+    protected $username, $password, $tenantName, $region, $token;
+    protected $computeClient, $identityClient, $serviceCatalog;
+
     /**
      * Factory method to create a new OpenstackClient
      *
@@ -27,9 +29,13 @@ class OpenstackClient extends \Guzzle\Service\Client{
      *
      * @return OpenstackClient
      */
-    public static function factory($config)
-    {       
-        $default = array();
+    public static function factory($config) {
+        $default = array(
+            'compute_type' => 'compute',
+            'identity_type' => 'identity',
+            'storage_type' => 'storage',
+            'region' => 'RegionOne'
+        );
         $required = array('auth_url');
         $config = Inspector::prepareConfig($config, $default, $required);
 
@@ -38,18 +44,16 @@ class OpenstackClient extends \Guzzle\Service\Client{
 
         return $client;
     }
-    
+
     /**
      * OpenstackClient constructor
      * 
      * @param string $auth_url URL of the Identity Service
      */
-    public function __construct($auth_url)
-    {
+    public function __construct($auth_url) {
         parent::__construct($auth_url);
     }
-    
-    
+
     /**
      * Authentication method
      * 
@@ -57,37 +61,82 @@ class OpenstackClient extends \Guzzle\Service\Client{
      * @param string $password Password
      * @param string $tenantName Tenant Name
      */
-    public function authenticate($username, $password, $tenantName)
-    {
+    public function authenticate($username, $password, $tenantName) {
         try {
-            $command = $this->getCommand('Authenticate', array('username'=>$username, 
-                'password'=>$password, 'tenantName'=>$tenantName));
+            $command = $this->getCommand('Authenticate', array('username' => $username,
+                'password' => $password, 'tenantName' => $tenantName));
             $authResult = $command->execute()->getResult();
-        } catch(BadResponseException $e){
+        } catch (BadResponseException $e) {
             throw new OpenstackException($e);
         }
-        
+
         $this->username = $username;
         $this->password = $password;
         $this->tenantName = $tenantName;
-        
+
         //Copy Service Catalog
         $this->serviceCatalog = $authResult['access']['serviceCatalog'];
-        
+
         //Get token
         $this->token = $authResult['access']['token'];
         
+        //Default Region
+        $this->region = 'RegionOne';
     }
-    
-    public function getServiceCatalog()
-    {
+
+    public function getServiceCatalog() {
         return $this->serviceCatalog;
     }
-    
-    public function getToken()
-    {
+
+    public function getToken() {
         return $this->token;
     }
+
+    /**
+     * Get endpoints for the service type for all regions
+     * 
+     * @param string $serviceType
+     * @return array 
+     */
+    private function getEndpoints($serviceType) {
+        $serviceEndpoints = array();
+        foreach ($this->serviceCatalog as $value) {
+            if ($value['type'] == $serviceType) {
+                $serviceEndpoints = $value['endpoints'];
+            }
+        }
+        return $serviceEndpoints;
+    }
+
+    /**
+     * Get an endpoint for a specific service and region
+     * 
+     * @param string $serviceType
+     * @param string $region
+     * @param string $endpointType
+     * @return string
+     */
+    private function getEndpoint($serviceType, $region, $endpointType='public') {
+        $serviceEndpoints = $this->getEndpoints($serviceType);
+        foreach ($serviceEndpoints as $endpointsRegion => $endpoints) {
+            if ($endpointsRegion == $region) {
+                return $endpoints[$endpointType . 'URL'];
+            }
+        }
+    }
+
+    public function getIdentityClient() {
+        if (!get_class($this->identityClient) == 'IdentityClient') {
+            $this->identityClient = IdentityClient::factory(array(
+                        'username' => $this->username,
+                        'password' => $this->password,
+                        'tenantName' => $this->tenantName,
+                        'base_url' => $this->getEndpoint('identity', $this->region, $endpointType)
+                    ));
+        }
+        return $this->identityClient;
+    }
+
 }
 
 ?>
